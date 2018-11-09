8c93b0903246a0eb175551a1f1509d2ae4489b5e
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-11-08 22:59:25 -0600

    full stack grocery list project

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const db = pgp(connectionString)
 const models = require('./models') //sequelize config
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('css'))
+app.use(express.static('images'))
+app.use(express.static('scss'))
+app.use(express.static('js'))
+app.use(express.static('fonts'))
+app.use(express.static('vendor/jquery'))
 
 app.engine('mustache',mustacheExpress())
 app.set('views','./views')
@@ -22,13 +27,44 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
-app.listen(3001,function(req,res){
+app.listen(3007,function(req,res){
   console.log("Server has started...")
 })
 //------------------------------------------------------
+//--------middleware------------------
+let authenticateLogin = function(req,res,next) {
+
+ // check if the user is authenticated
+ if(req.session.username) {
+   next()
+ } else {
+   res.redirect("/login")
+ }
+
+}
+app.all("/",authenticateLogin,function(req,res,next){
+   next()
+})
+//---------------------------------
 app.get('/',function(req,res){
-  models.store.findAll().then(function(stores){
-    res.render('index',{stores:stores})
+
+  models.store.findAll({
+    where: {
+      userid: req.session.userId
+    },
+    include:[
+      {
+        model : models.item,
+        as: 'items'
+      }
+    ]
+  }).then(function(stores){
+    if(req.session.username){
+      res.render('index',{stores:stores, username:req.session.username})
+    } else{
+      res.render('index',{stores:stores})
+    }
+
   })
 
 })
@@ -37,8 +73,9 @@ app.post('/add_store',function(req,res){
   let street = req.body.street
   let city = req.body.city
   let state = req.body.state
+  let userid = req.session.userId
 let store = models.store.build(
-  {name:name, street:street,city:city,state:state}
+  {name:name, street:street,city:city,state:state, userid : userid}
 )
 store.save().then(function(){
   res.redirect('/')
@@ -54,4 +91,74 @@ app.get('/delete/:id',function(req,res){
     res.redirect('/')
   })
 
+})
+app.post('/add_item',function(req,res){
+  let item = req.body.item
+  let storeId = req.body.storeId
+  console.log("store id is " + storeId)
+   models.item.build({
+    name : item,
+    storeid: storeId
+  }).save().then(function(newItem){
+    console.log(newItem)
+    res.redirect('/')
+  })
+  .catch(function(error){
+    console.log(error)
+  })
+
+
+})
+app.get('/deleteItem/:id', function(req,res){
+  let itemId = req.params.id
+  models.item.findById(itemId).then(function(item){
+    return item.destroy()
+  }).then(function(){
+    res.redirect('/')
+  })
+})
+app.get('/login',function(req,res){
+  res.render('login')
+})
+app.post('/signup',function(req,res){
+  let username = req.body.username
+  let email = req.body.email
+  let password = req.body.password
+  console.log(password)
+  models.user.build({
+    username:username,
+    email:email,
+    password:password
+  }).save().then(function(){
+    res.redirect('/login')
+  }).catch(function(error){
+    console.log(error)
+    alert(error)
+  })
+})
+app.post('/login', function(req,res){
+  let username = req.body.your_username
+  let password = req.body.your_password
+ let user = models.user.findOne({
+  where:{
+    username:username,
+    password: password
+  }
+}).then(function(user){
+  if(user != null){
+    req.session.username = user.username
+    req.session.userId = user.id
+    console.log(req.session.userId)
+    res.redirect('/')
+  } else {
+    res.redirect('/login')
+  }
+}).catch(function(error){
+  res.redirect('/login')
+})
+
+})
+app.get('/logout',function(req,res){
+  req.session.destroy()
+  res.redirect('/login')
 })
